3d825c9eecec1b8e4760030eeaebe442663b43d6
Greg Vdb <vdb_g@hotmail.com>
2022-08-25 14:42:07 -0700

    add polygon geofences

--- geofence.py
+++ geofence.py
@@ -1,11 +1,41 @@
-# https://github.com/geopy/geopy
-from geopy.distance import Distance
-from geopy.point import Point
-from geopy.distance import great_circle
-
+import math
 import logging
 
 from hubitat import Hubitat
+
+
+class Point:
+    def __init__(self, latitude: float, longitude: float) -> None:
+        longitude = float(longitude)
+        latitude = float(latitude)
+        if longitude < -180 or longitude > 180:
+            raise Exception(f"Invalid longitude value {longitude}.")
+        if latitude < -90 or latitude > 90:
+            raise Exception(f"Invalid latitude value {latitude}.")
+
+        self.latitude = latitude
+        self.longitude = longitude
+        self.projection = None
+
+    # Copied from https://community.esri.com/t5/python-questions/lat-long-to-web-mercator/td-p/521492
+    def getWebMercator(self) -> list[float]:
+        if not self.projection:
+            num = self.longitude * 0.017453292519943295
+            x = 6378137.0 * num
+            a = self.latitude * 0.017453292519943295
+            x_mercator = x
+            y_mercator = 3189068.5 * math.log((1.0 + math.sin(a)) / (1.0 - math.sin(a)))
+            self.projection = x_mercator, y_mercator
+        return self.projection
+
+    def X(self) -> float:
+        return self.getWebMercator()[0]
+
+    def Y(self) -> float:
+        return self.getWebMercator()[1]
+
+    def __str__(self) -> str:
+        return f"(lat={self.latitude}.long={self.longitude};x={self.X()},y={self.Y()})"
 
 
 class Geofence:
@@ -46,6 +76,41 @@ class Geofence:
         hubitat.set_presence(id=self.tiles[key], arrived=self.isInside(p))
 
 
+class PolygonFence(Geofence):
+    def __init__(self, name: str, conf: dict, all_tiles: set[str], hubitatIds: set[int], hubitat_devices: dict[int, str]) -> None:
+        super().__init__(name, conf, all_tiles, hubitatIds, hubitat_devices)
+
+        vertices: list[list[float]] = conf.get("vertices", [])
+
+        if len(vertices) < 3:
+            raise Exception(f"Polygon fence '{self.name}' needs at least 3 vertices.")
+
+        self.p: list[Point] = []
+
+        for data in vertices:
+            if len(data) != 2:
+                raise Exception(f"Invalid (lat,long) pair: {data}")
+            self.p.append(Point(data[0], data[1]))
+
+    # Copied from http://alienryderflex.com/polygon/
+    def pointInPolygon(self, x: float, y: float) -> bool:
+        corners: int = len(self.p)
+        i: int = 0
+        j: int = corners - 1
+        ret: bool = False
+
+        while i < corners:
+            if (self.p[i].Y() < y and self.p[j].Y() >= y or self.p[j].Y() < y and self.p[i].Y() >= y) and (self.p[i].X() <= x or self.p[j].X() <= x):
+                ret ^= (self.p[i].X()+(y-self.p[i].Y())/(self.p[j].Y()-self.p[i].Y())*(self.p[j].X()-self.p[i].X()) < x)
+            j = i
+            i = i+1
+
+        return ret
+
+    def isInside(self, p: Point) -> bool:
+        return self.pointInPolygon(p.X(), p.Y())
+
+
 class CircleFence(Geofence):
     def __init__(self, name: str, conf: dict, all_tiles: set[str], hubitatIds: set[int], hubitat_devices: dict[int, str]) -> None:
         super().__init__(name, conf, all_tiles, hubitatIds, hubitat_devices)
@@ -54,11 +119,26 @@ class CircleFence(Geofence):
         radius: int = conf["radius"]
         self.center = Point(latitude=latitude, longitude=longitude)
         logging.debug(f"CircleFence(name={name},lat={self.center.latitude},long={self.center.longitude},radius={radius}m)")
-        self.radius = Distance(kilometers=radius / 1000)
+        self.radius = radius
+
+    # Computed using https://en.wikipedia.org/wiki/Haversine_formula
+    def getDistance(self, p1: Point, p2: Point) -> float:
+        # convert to radians
+        lonp1 = math.radians(p1.longitude)
+        lonp2 = math.radians(p2.longitude)
+        latp1 = math.radians(p1.latitude)
+        latp2 = math.radians(p2.latitude)
+
+        dlon = lonp2 - lonp1
+        dlat = latp2 - latp1
+        a = math.sin(dlat/2)**2 + math.cos(latp1) * math.cos(latp2) * math.sin(dlon/2)**2
+        c = 2 * math.asin(math.sqrt(a))
+        r = 6371  # https://en.wikipedia.org/wiki/Earth_radius
+        return c * r * 1000  # return in meters
 
     def isInside(self, p: Point) -> bool:
-        distance = great_circle(self.center, p)
-        logging.debug(f"Distance to {self.name}: {distance.m} m")
+        distance = self.getDistance(self.center, p)
+        logging.debug(f"Distance to '{self.name}': {distance} m")
         return distance <= self.radius
 
 
@@ -72,7 +152,10 @@ class Geofences:
             for name, data in conf["circles"].items():
                 self.geofences.append(CircleFence(name, data, self.tiles, self.hubitatIds, hubitat_devices))
 
-    def evaluate(self, longitude: float, latitude: float, name: str, uuid: str, hubitat: Hubitat) -> bool:
-        p: Point = Point(latitude=latitude, longitude=longitude)
+        if "polygons" in conf:
+            for name, data in conf["polygons"].items():
+                self.geofences.append(PolygonFence(name, data, self.tiles, self.hubitatIds, hubitat_devices))
+
+    def evaluate(self, p: Point, name: str, uuid: str, hubitat: Hubitat) -> bool:
         for geofence in self.geofences:
             geofence.processTile(p=p, name=name, uuid=uuid, hubitat=hubitat)
